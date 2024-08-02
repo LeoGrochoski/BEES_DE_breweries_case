b31250f84e8f4b8bcc7c3322fab637d1b6cde99f
LeoGrochoski <leogrochoski10@gmail.com>
2024-08-01 23:30:56 -0300

    fix: corrigindo problema de particionamento pelo estado

--- scripts/transformacao_dados.py
+++ scripts/transformacao_dados.py
@@ -5,6 +5,7 @@ import boto3
 import logging
 from dotenv import load_dotenv
 import os
+from datetime import datetime
 
 # Configuração de logging
 logging.basicConfig(
@@ -49,14 +50,14 @@ def transformacao_dados(entrada_bucket, saida_bucket, prefix):
         aws_secret_access_key=AWS_SECRET_ACCESS_KEY
     )
 
-    # Garantindo que o diretórioo temp exista
+    # Garantindo que o diretório tmp exista
     if not os.path.exists('/tmp'):
         os.makedirs('/tmp')
 
     try:
         logging.info("Procurando o último arquivo CSV no S3")
         entrada_arq = pegar_ultimo_arquivo_csv(entrada_bucket, prefix)
-        logging.info(f"Ultimo arquivo CSV encontrado: {entrada_arq}")
+        logging.info(f"Último arquivo CSV encontrado: {entrada_arq}")
     except Exception as e:
         logging.error(f"Erro ao procurar o último arquivo CSV: {e}")
         return
@@ -72,38 +73,51 @@ def transformacao_dados(entrada_bucket, saida_bucket, prefix):
     try:
         logging.info("Lendo arquivo CSV em forma de DF")
         df = pd.read_csv(caminho_temporario)
+
+        # Normalizando e removendo espaços em branco na coluna 'state', evitando erros humano
+        df['state'] = df['state'].str.strip().str.title()
+        df = df.dropna(subset=['state'])
     except Exception as e:
         logging.error(f"Erro ao ler arquivo CSV: {e}")
         return
     
     try:
-        logging.info("Convertendo DataFrame para uma tabela em formato Apache Arrow")
-        tabela_arrow = pa.Table.from_pandas(df)
+        logging.info("Agrupando dados por estado")
+        grouped = df.groupby('state')
     except Exception as e:
-        logging.error(f"Erro convertendo DataFrame para tabela Arrow: {e}")
+        logging.error(f"Erro ao agrupar dados por estado: {e}")
         return
     
+    for state, group in grouped:
+        try:
+            timestamp = datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
+            nome_saida_arq = f"{state.replace(' ', '_')}_{timestamp}.parquet"
+            caminho_saida = os.path.join('/tmp', nome_saida_arq)
+            
+            logging.info(f"Processando dados para o estado: {state}")
+            tabela_arrow = pa.Table.from_pandas(group)
+            
+            logging.info(f"Escrevendo dados para {state} em Parquet")
+            pq.write_table(tabela_arrow, caminho_saida, compression='snappy')
+            
+            logging.info(f"Subindo arquivo Parquet para o estado: {state}")
+            s3_path = os.path.join(prefix, nome_saida_arq)
+            conexao_s3.upload_file(caminho_saida, saida_bucket, s3_path)
+        
+        except Exception as e:
+            logging.error(f"Erro ao processar o estado {state}: {e}")
+            continue
+
+    # Limpeza dos arquivos temporários, estava tendo problema com qualidade de dados
     try:
-        logging.info("Escrevendo Tabela Arrow em Parquet com particionamento")
-        nome_saida_arq = entrada_arq.replace('.csv', '')
-        caminho_saida = os.path.join('/tmp', nome_saida_arq)
-        pq.write_to_dataset(tabela_arrow, root_path=caminho_saida, partition_cols=['state'])
-    except Exception as e:
-        logging.error(f"Erro ao escrever arquivo Parquet: {e}")
-        return
-    
-    try:
-        logging.info("Subindo Parquet particionado para S3")
-        for root, _, files in os.walk(caminho_saida):
+        logging.info("Removendo arquivos temporários")
+        for root, dirs, files in os.walk('/tmp'):
             for file in files:
-                if file.endswith(".parquet"):
-                    caminho_completo = os.path.join(root, file)
-                    s3_path = os.path.relpath(caminho_completo, '/tmp')
-                    conexao_s3.upload_file(caminho_completo, saida_bucket, s3_path)
+                os.remove(os.path.join(root, file))
     except Exception as e:
-        logging.error(f"Erro ao enviar Parquet para S3: {e}")
+        logging.error(f"Erro ao remover arquivos temporários: {e}")
         return
 
-    logging.info("Transformacao dos dados completa")
+    logging.info("Transformação dos dados completa")
 
 transformacao_dados(BUCKET_LAND, BUCKET_RAW, 'breweries_data_land_')
