a6a51371f927861992a1caae1ffe37945a8db4fb
LeoGrochoski <leogrochoski10@gmail.com>
2024-08-07 13:40:53 -0300

    fix: corrigindo dash

--- app/dashboard.py
+++ app/dashboard.py
@@ -6,21 +6,19 @@ import os
 from dotenv import load_dotenv
 import plotly.express as px
 
-load_dotenv() 
+# Carregar variáveis de ambiente
+load_dotenv()
 
-# Função para baixar o arquivo Parquet do S3
 def baixar_arquivo_parquet(bucket_name: str, file_key: str, local_path: str):
     s3 = boto3.client('s3',
                       aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
                       aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
     s3.download_file(bucket_name, file_key, local_path)
 
-# Carregar dados do arquivo Parquet
 def carregar_dados(local_path: str) -> pd.DataFrame:
     table = pq.read_table(local_path)
     return table.to_pandas()
 
-# Função principal do Streamlit
 def main():
     st.title("Dashboard de Cervejarias")
 
@@ -29,16 +27,20 @@ def main():
     TEMP_DIR = os.getenv('TEMP_DIR')
     FILE_KEY = 'aggregated_breweries.parquet'
 
-    st.write(f"BUCKET_CURATED: {BUCKET_CURATED}")
-    st.write(f"TEMP_DIR: {TEMP_DIR}")
+    # Verificar se as variáveis de ambiente estão configuradas corretamente
+    if not BUCKET_CURATED:
+        st.error("A variável de ambiente BUCKET_CURATED não está configurada.")
+        return
+    
+    if not TEMP_DIR:
+        st.error("A variável de ambiente TEMP_DIR não está configurada.")
+        return
 
     # Definir o LOCAL_PATH para o diretório temporário
     LOCAL_PATH = os.path.join(TEMP_DIR, 'aggregated_breweries.parquet')
 
-    # Verificar se as variáveis de ambiente estão configuradas corretamente
-    if not BUCKET_CURATED or not TEMP_DIR:
-        st.error("As variáveis de ambiente BUCKET_CURATED ou TEMP_DIR não estão configuradas.")
-        return
+    st.write(f"BUCKET_CURATED: {BUCKET_CURATED}")
+    st.write(f"TEMP_DIR: {TEMP_DIR}")
 
     # Baixar o arquivo Parquet do S3
     baixar_arquivo_parquet(BUCKET_CURATED, FILE_KEY, LOCAL_PATH)
